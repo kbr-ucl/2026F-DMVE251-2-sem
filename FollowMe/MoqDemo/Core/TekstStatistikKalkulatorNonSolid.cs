252646f2a54f9e73a40d5c8767444676f587d5c6
Kaj Bromose <kbr@ucl.dk>
2026-02-26 08:55:55 +0100

    TekstStatistikKalkulator - før follow-me

---
+++ FollowMe/MoqDemo/Core/TekstStatistikKalkulatorNonSolid.cs
@@ -0,0 +1,30 @@
+﻿namespace Core
+{
+    public class TekstStatistikKalkulatorNonSolid
+    {
+        public TekstStatistik BeregnTekstStatistik(string fileNavn)
+        {
+            // Indlæs fil
+            string[] lines = File.ReadAllLines(fileNavn);
+
+            // Beregn statistik
+            int antalLinjer = lines.Length;
+            int antalAnslag = 0;
+            int antalOrd = 0;
+
+            foreach (var line in lines)
+            {
+                antalAnslag += line.Length;
+                antalOrd += line.Split(' ').Length;
+            }
+
+            // returner statistik
+            return new TekstStatistik(antalAnslag, antalOrd, antalLinjer);
+        }
+
+
+    }
+
+    public record TekstStatistik(int AntalAnslag, int AntalOrd, int AntalLinjer);
+
+}
