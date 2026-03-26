7fe71b1341b581433232c6e2f5af69965f5a60a7
Kaj Bromose <kbr@ucl.dk>
2026-03-26 10:50:02 +0100

    IoC followme

---
+++ FollowMe/ConsoleAppDiExample/ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection()
+    .AddTransient<IGreetingService, GreetingService>()
+    .AddTransient<MyFancyGreetingService>()
+    .AddTransient<MyApplication>()
+    .BuildServiceProvider();
+
+var greetingService = services.GetRequiredService<MyApplication>();
+greetingService.Run();
+
+public interface IGreetingService
+{
+    void Greet(string name);
+}
+
+public class GreetingService : IGreetingService
+{
+    void IGreetingService.Greet(string name)
+    {
+        Console.WriteLine($"Hej, {name}");
+    }
+}
+
+public class MyApplication
+{
+    private readonly IGreetingService _greetingService;
+
+    public MyApplication(IGreetingService greetingService)
+    {
+        _greetingService = greetingService;
+    }
+
+    public void Run()
+    {
+        _greetingService.Greet("Datamatiker");
+    }
+
+    public string Run2()
+    {
+        return "Hello from IOC";
+    }
+}
+
+public class MyFancyGreetingService(IGreetingService greetingService)
+{
+    public void Run()
+    {
+        greetingService.Greet("Datamatiker");
+    }
+}
